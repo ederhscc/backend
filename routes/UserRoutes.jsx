dd80b117abd4f7fc0064362e623a22df1ef41eda
Eder Henrique <ederhscc@gmail.com>
2023-09-22 14:41:34 -0300

    added UserController and UserRoutes

---
+++ routes/UserRoutes.jsx
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Controller
+const { register } = require("../controllers/UserController");
+
+router.post("/register", register);
+
+module.exports = router;
