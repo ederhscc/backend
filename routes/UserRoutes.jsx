aa5880d74813730271bb24579c5b7df06004de2a
Eder Henrique <ederhscc@gmail.com>
2023-09-26 09:16:50 -0300

    added login validation

--- routes/UserRoutes.jsx
+++ routes/UserRoutes.jsx
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { register } = require("../controllers/UserController.jsx");
+const { register, login } = require("../controllers/UserController.jsx");
 
 // Middlewares
 const validate = require("../middlewares/handleValidation.jsx");
-const {userCreateValidation} = require("../middlewares/userValidations.jsx")
+const {
+  userCreateValidation,
+  loginValidation,
+} = require("../middlewares/userValidations.jsx");
 
+// Routes
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
 
 module.exports = router;
