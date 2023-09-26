94c4632ae27a0503ad77690828050517706946f2
Eder Henrique <ederhscc@gmail.com>
2023-09-26 10:14:57 -0300

    added function for rescue authenticated user

--- routes/UserRoutes.jsx
+++ routes/UserRoutes.jsx
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { register, login } = require("../controllers/UserController.jsx");
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/UserController.jsx");
 
 // Middlewares
 const validate = require("../middlewares/handleValidation.jsx");
@@ -10,9 +14,11 @@ const {
   userCreateValidation,
   loginValidation,
 } = require("../middlewares/userValidations.jsx");
+const authGuard = require("../middlewares/authGuard.jsx");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
 
 module.exports = router;
