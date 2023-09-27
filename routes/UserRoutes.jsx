2565fd64fdd8c3f6940504c36cce6e1ebe155ddc
Eder Henrique <ederhscc@gmail.com>
2023-09-27 10:21:29 -0300

    added update an user

--- routes/UserRoutes.jsx
+++ routes/UserRoutes.jsx
@@ -6,6 +6,7 @@ const {
   register,
   login,
   getCurrentUser,
+  update,
 } = require("../controllers/UserController.jsx");
 
 // Middlewares
@@ -13,12 +14,22 @@ const validate = require("../middlewares/handleValidation.jsx");
 const {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 } = require("../middlewares/userValidations.jsx");
 const authGuard = require("../middlewares/authGuard.jsx");
+const { imageUpload } = require("../middlewares/imageUpload.jsx");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 module.exports = router;
