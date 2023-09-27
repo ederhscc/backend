9f322edb0dcc26ac122ebe359406bbd960aeaaed
Eder Henrique <ederhscc@gmail.com>
2023-09-27 14:28:36 -0300

    added get user by id route

--- controllers/UserController.jsx
+++ controllers/UserController.jsx
@@ -87,7 +87,7 @@ const getCurrentUser = async (req, res) => {
 // Update user
 const update = async (req, res) => {
   const { name, password, bio } = req.body;
-  
+
   let profileImage = null;
 
   if (req.file) {
@@ -96,9 +96,9 @@ const update = async (req, res) => {
 
   const reqUser = req.user;
 
-  const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select(
-    "-password"
-  );
+  const user = await User.findById(
+    new mongoose.Types.ObjectId(reqUser._id)
+  ).select("-password");
 
   if (name) {
     user.name = name;
@@ -123,9 +123,32 @@ const update = async (req, res) => {
   res.status(200).json(user);
 };
 
+// Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(new mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+
+    // Check if user exists
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+};
+
 module.exports = {
   register,
   login,
   getCurrentUser,
   update,
+  getUserById,
 };
