33af3d5cbd534a62d5c82d999ec520fcae5358c2
Eder Henrique <ederhscc@gmail.com>
2023-09-29 11:34:49 -0300

    added delete a photo function

--- routes/PhotoRoutes.jsx
+++ routes/PhotoRoutes.jsx
@@ -2,22 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const {insertPhoto} = require("../controllers/PhotoController.jsx")
+const {
+  insertPhoto,
+  deletePhoto,
+} = require("../controllers/PhotoController.jsx");
 
 // Middlewares
 const { photoInsertValidator } = require("../middlewares/photoValidations.jsx");
 const authGuard = require("../middlewares/authGuard.jsx");
 const validate = require("../middlewares/handleValidation.jsx");
-const {imageUpload} = require("../middlewares/imageUpload.jsx")
+const { imageUpload } = require("../middlewares/imageUpload.jsx");
 
 // Routes
 router.post(
-    "/",
-    authGuard,
-    imageUpload.single("image"),
-    photoInsertValidator(),
-    validate,
-    insertPhoto
-)
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidator(),
+  validate,
+  insertPhoto
+);
+router.delete("/:id", authGuard, deletePhoto);
 
 module.exports = router;
