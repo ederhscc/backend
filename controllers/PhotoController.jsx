3e42c67817c8fb03041f10beb04f239ac807d03d
Eder Henrique <ederhscc@gmail.com>
2023-10-02 11:21:12 -0300

    added comments in photo

--- controllers/PhotoController.jsx
+++ controllers/PhotoController.jsx
@@ -171,19 +171,58 @@ const likePhoto = async (req, res) => {
     // Put user id in likes array
     photo.likes.push(reqUser._id);
 
-    photo.save();
+    await photo.save();
 
-    res
-      .status(200)
-      .json({
-        photoId: id,
-        userId: reqUser._id,
-        message: "A foto foi curtida!",
-      });
+    res.status(200).json({
+      photoId: id,
+      userId: reqUser._id,
+      message: "A foto foi curtida!",
+    });
   } catch (error) {
     res.status(404).json({
       errors: ["Foto não encontrada."],
     });
+    return;
+  }
+};
+
+// Comment functonality
+const commentPhoto = async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+
+  const reqUser = req.user;
+
+  try {
+    const user = await User.findById(reqUser._id);
+
+    const photo = await Photo.findById(id);
+
+    // Check if photo exists
+    if (!photo) {
+      res.status(404).json({ errors: ["Foto não encontrada."] });
+      return;
+    }
+
+    // Put comment in the array comments
+    const userComment = {
+      comment,
+      userName: user.name,
+      userImage: user.profileImage,
+      userId: user._id,
+    };
+
+    // Put comments in comments array
+    photo.comments.push(userComment);
+
+    await photo.save();
+
+    res.status(200).json({
+      comment: userComment,
+      message: "O comentário foi adicionado com sucesso!",
+    });
+  } catch (error) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
     return;
   }
 };
@@ -196,4 +235,5 @@ module.exports = {
   getPhotoById,
   updatePhoto,
   likePhoto,
+  commentPhoto,
 };
--- routes/PhotoRoutes.jsx
+++ routes/PhotoRoutes.jsx
@@ -10,12 +10,14 @@ const {
   getPhotoById,
   updatePhoto,
   likePhoto,
+  commentPhoto,
 } = require("../controllers/PhotoController.jsx");
 
 // Middlewares
 const {
   photoInsertValidator,
   photoUpdateValidation,
+  commentValidation,
 } = require("../middlewares/photoValidations.jsx");
 const authGuard = require("../middlewares/authGuard.jsx");
 const validate = require("../middlewares/handleValidation.jsx");
@@ -36,5 +38,6 @@ router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
+router.put("/comment/:id", authGuard, commentValidation(), validate, commentPhoto);
 
 module.exports = router;
